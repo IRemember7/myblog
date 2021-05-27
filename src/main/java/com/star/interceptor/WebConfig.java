90a1d7da276893e907f88a1795a86953048d8af4
IRemember7 <997918253@qq.com>
2021-05-27 19:42:57 +0800

    初始化

---
+++ src/main/java/com/star/interceptor/WebConfig.java
@@ -0,0 +1,31 @@
+package com.star.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @Description: 指定拦截内容的配置类
+ * @Author: ONESTAR
+ * @Date: Created in 13:57 2020/3/27
+ * @QQ群: 530311074
+ * @URL: https://onestar.newstar.net.cn/
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/adminMain").setViewName("admin/index");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin")
+                .excludePathPatterns("/admin/login");
+    }
+
+}
